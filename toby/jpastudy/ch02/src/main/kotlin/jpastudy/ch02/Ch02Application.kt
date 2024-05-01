73ca71ffae15beed52141bd14c103248e0167b61
eddie <sangwoo98.kang@gmail.com>
2024-05-01 14:31:21 +0900

    fix: Conflict 해결

---
+++ toby/jpastudy/ch02/src/main/kotlin/jpastudy/ch02/Ch02Application.kt
@@ -0,0 +1,11 @@
+package jpastudy.ch02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch02Application
+
+fun main(args: Array<String>) {
+    runApplication<Ch02Application>(*args)
+}
